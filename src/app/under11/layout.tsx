97a084f59ab47aa5ae930cb1a7e6d96734e770ef
RohitSah23 <rohitisog@gmail.com>
2025-09-22 23:08:53 +0530

    added layout of under11

---
+++ src/app/under11/layout.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from "next";
+import { Nunito_Sans, Oswald } from "next/font/google";
+import "./globals.css";
+
+// Body font
+const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // optional, choose needed weights
+});
+
+// Heading font
+const oswald = Oswald({
+  variable: "--font-oswald",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // optional, choose needed weights
+});
+
+export const metadata: Metadata = {
+  title: "Taplow United FC",
+  description:
+    "Taplow United FC, established in 1923, offers football opportunities for all ages. Join our teams, view fixtures, or contact us for more information.",
+  openGraph: {
+    title: "Taplow United FC",
+    description:
+      "Taplow United FC, established in 1923, offers football opportunities for all ages. Join our teams, view fixtures, or contact us for more information.",
+    url: "https://www.taplow-utd.co.uk/",
+    siteName: "Taplow United FC",
+    images: [
+      {
+        url: "https://www.taplow-utd.co.uk/images/logo.png", 
+        width: 800,
+        height: 800,
+        alt: "Taplow United FC Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Taplow United FC",
+    description:
+      "Taplow United FC, established in 1923, offers football opportunities for all ages. Join our teams, view fixtures, or contact us for more information.",
+    images: ["https://www.taplow-utd.co.uk/images/logo.png"], 
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${nunitoSans.variable} ${oswald.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
